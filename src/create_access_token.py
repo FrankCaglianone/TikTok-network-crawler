30494da8c8456699664dad5f77ef76c5e6d23dbc
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-18 12:27:50 +0100

    Fixed the create_access_token.py added failed authentication check

--- src/create_access_token.py
+++ src/create_access_token.py
@@ -6,10 +6,8 @@ auth_url = 'https://open.tiktokapis.com/v2/oauth/token/'
 
 # Get key and secret from the standard input
 print("\n")
-print("Please insert your key")
-client_key = input()
-print("Please insert your secret")
-client_secret = input()
+client_key = input("Please insert your key: ")
+client_secret = input("Please insert your secret: ")
 
 
 # Creating the header
@@ -33,13 +31,16 @@ auth_response = requests.post(auth_url, headers = head, data = body)
 
 # Check if the request was successful
 if auth_response.status_code == 200:
-    token = auth_response.json().get('access_token')
-    expiration = auth_response.json().get('expires_in')
-    type = auth_response.json().get('token_type')
+    if auth_response.json().get('access_token') is None:
+        print('Authentication failed, please check your credentials')
+    else:
+        token = auth_response.json().get('access_token')
+        expiration = auth_response.json().get('expires_in')
+        type = auth_response.json().get('token_type')
 
-    print("\n")
-    print("Access Token: ", token)
-    print("Expiration in seconds: ", expiration)
-    print("Token Type: ", type)
+        print("\n")
+        print("Access Token: ", token)
+        print("Expiration in seconds: ", expiration)
+        print("Token Type: ", type)
 else:
     print("Failed to obtain access token. Status code:", auth_response.status_code)
