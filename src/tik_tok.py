e0c55fe05cf61673f4cc49f78422a5b60a3eab5c
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-18 15:17:08 +0100

    extracted the followers list from the json response

--- src/tik_tok.py
+++ src/tik_tok.py
@@ -5,6 +5,7 @@ url = "https://open.tiktokapis.com/v2/research/user/followers/"
 
 
 # Get the Access Token from stdin
+print("\n")
 access_token = input("Enter your access token: ")
 auth = "Bearer " + access_token
 
@@ -32,8 +33,28 @@ response = requests.post(url=url, headers=header, json=dat)
 
 
 
+# Response structure
+# "data": {
+#     "cursor": 1706837834,
+#     "has_more": true,
+#     "user_followers": [
+#         {
+#             "display_name": "test user",
+#             "username": "test_username"
+#         },
+#         {
+#             "username": "test user 2",
+#             "display_name": "test_username2"
+#         }
+#     ]
+# },
+
+# 
 if response.status_code == 200:
-    followers = response.json()
-    print(followers)
+    data = response.json().get('data')
+    followers_list = data.get('user_followers')
+
+    print("\n\n")
+    print(followers_list)
 else:
     print("Failed to retrieve followers. Status code:", response.status_code)
