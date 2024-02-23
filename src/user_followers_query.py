20e1e3664d2559a78f91b96f1d3dd0e7dc9ffe43
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-23 12:12:00 +0100

    Fixed OOP and created a dictionary populating it succesfully

--- src/user_followers_query.py
+++ src/user_followers_query.py
@@ -1,20 +1,5 @@
 import requests
 
-
-'''
-    This class is used to interact with the TikTok Research API to Query User Followers.
-    It makes a post request to the server and returns data in JSON format.
-    From that it gets from the JSON response the list of all the followers of the given username.
-
-
-    Stdin: 
-        - access_token: The access token to acces the TikTok Research API is required
-        - starting_username: The username of the account you want to query from
-    
-    Stdout: 
-'''
-
-
 '''
     Response structure example
 
@@ -37,7 +22,19 @@ import requests
 
 
 
-def main(): 
+
+
+# Helper functions
+def print_dictionary():
+    print("Username to Parsed Status:")
+    for username, parsed in parsing_list.items():
+        print(f"{username}: {parsed}")
+
+
+
+
+# DOCSTRING
+def get_response(): 
     # API url
     url = "https://open.tiktokapis.com/v2/research/user/followers/"
 
@@ -56,26 +53,68 @@ def main():
 
 
     # Get the starting username from stdin
+    global starting_username
     starting_username = input("Please enter the TikTok Username: ")
-                          
+                        
 
     # Creating the body 
-    dat = {
+    data = {
         "username": starting_username,  
         "max_count": 10,  # Optional: Adjust the number of results as needed
     }
 
 
     # Make the post request
-    response = requests.post(url=url, headers=header, json=dat)
+    response = requests.post(url=url, headers=header, json=data)
 
 
     # Get the response
     if response.status_code == 200:
         data = response.json().get('data')
-        followers_list = data.get('user_followers')
-
-        print("\n")
-        print(followers_list)
+        return data
     else:
         print("Failed to retrieve followers. Status code:", response.status_code)
+
+
+    
+# DOCSTRING
+def populate_parsing_list(followers_list):
+    for user in followers_list:
+        username = user["username"]
+
+        if username in parsing_list:
+            continue
+        else:
+            parsing_list[username] = 0
+
+
+
+
+
+
+   
+# Declaring Global Variables
+starting_username = None
+parsing_list = {}  # Maps username to parsed bit (0 or 1)
+
+
+
+
+def main():
+    res = get_response()
+    followers_list = res.get('user_followers')
+
+    # Add the starting username
+    parsing_list[starting_username] = 1
+
+    populate_parsing_list(followers_list)
+    print_dictionary()
+
+
+
+
+
+
+
+main()
+   
