6ab4c02eaf503432c6a6375151bb08d495c018c9
Francesco Caglianone <francescocaglianone@gmail.com>
2024-03-08 13:00:02 +0100

    dummy commit

--- src/user_following_query.py
+++ src/user_following_query.py
@@ -133,7 +133,7 @@ def get_all_followers(parsing_user):
         # Create body
         body = {
             "username": parsing_user,
-            "max_count": 10,
+            "max_count": 100,
         }
 
         # Add cursor to request if it exists
