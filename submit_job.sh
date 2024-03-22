8b2980df94a0650ff694de83a8baa48630e87134
Francesco Caglianone <francescocaglianone@gmail.com>
2024-03-22 16:54:34 +0100

    Created first version of job scheduling

---
+++ submit_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=python_network_query    # Job name
+#SBATCH --output=python_job_%j.out         # Output file. %j will be replaced with the job ID
+#SBATCH --cpus-per-task=4                  # Number of CPU cores per task
+#SBATCH --mem=4gb                          # Memory total in MB (for all cores)
+#SBATCH --time=02:00:00                    # Time limit hrs:min:sec
+#SBATCH --partition=brown                  # Run on either the Red or Brown queue
+#SBATCH --mail-type=FAIL,END               # Mail events (NONE, BEGIN, END, FAIL, ALL)
+
+# Load any modules or software you need
+# module load python3
+
+# Execute the Python script
+python3 src/main.py
