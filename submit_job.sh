4644662da565a790b95aa32fe9d2b1f77ed12ffa
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-12 19:50:41 +0200

    modified the job

--- submit_job.sh
+++ submit_job.sh
@@ -4,13 +4,12 @@
 #SBATCH --job-name=python_network_query    # Job name
 #SBATCH --output=python_job_%j.out         # Output file. %j will be replaced with the job ID
 #SBATCH --cpus-per-task=4                  # Number of CPU cores per task
-#SBATCH --mem=4gb                          # Memory total in MB (for all cores)
-#SBATCH --time=02:00:00                    # Time limit hrs:min:sec
+#SBATCH --mem=16gb                         # Memory total in MB (for all cores)
+#SBATCH --time=36:00:00                    # Time limit hrs:min:sec
 #SBATCH --partition=brown                  # Run on either the Red or Brown queue
-#SBATCH --mail-type=FAIL,END,BEGIN         # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-type=BEGIN,FAIL,END         # Mail events (NONE, BEGIN, END, FAIL)
+#SBATCH --mail-user=youremail@domain.com   # Email to send the mail events
 
-# Load any modules or software you need
-# module load python3
 
-# Execute the Python script
-python3 src/main.py
+
+python3 ./src/main.py key secret user_input
