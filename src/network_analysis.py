380ad59cad4e0417f5c78197e03281fd7a093c7d
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-19 16:28:53 +0200

    code cleaning

--- src/network_analysis.py
+++ src/network_analysis.py
@@ -27,71 +27,6 @@ def print_nodes_list():
 
 
 
-# Initialize an empty list to store edges
-edges = []
-
-
-# Read the CSV file
-with open('', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        edges.append(tuple(row))
-
-
-# Create a graph from the list of edges
-g = ig.Graph.TupleList(edges, directed=True)
-
-
-# Calculate the page ranking
-# TODO: pagerank = g.pagerank(damping=0.85)?
-pagerank = g.pagerank()
-
-
-
-# Pair ranks with usernames
-pagerank_list = []
-for vertex, score in zip(g.vs, pagerank):
-    pagerank_list.append((vertex['name'], score))
-
-
-# Sort the list
-# TODO: ascending or descending? -> # sorted_scores = sorted(pageranks_map.items(), key=lambda item: item[1], reverse=True)
-sorted_pageranks_list = sorted(pagerank_list, key=lambda item: item[1])
-
-
-
-
-
-scores = [score for name, score in sorted_pageranks_list]
-
-# Calculate percentiles
-p25 = np.percentile(scores, 25)
-p50 = np.percentile(scores, 50)
-p75 = np.percentile(scores, 75)
-
-# Initialize lists for each range
-range_0_25 = []
-range_26_50 = []
-range_51_75 = []
-range_76_100 = []
-
-# Assign data to each range based on scores
-for name, score in sorted_pageranks_list:
-    if score <= p25:
-        range_0_25.append((name, score))
-    elif p25 < score <= p50:
-        range_26_50.append((name, score))
-    elif p50 < score <= p75:
-        range_51_75.append((name, score))
-    elif score > p75:
-        range_76_100.append((name, score))
-
-# Display the results
-print("0% - 25% range:", range_0_25)
-print("26% - 50% range:", range_26_50)
-print("51% - 75% range:", range_51_75)
-print("76% - 100% range:", range_76_100)
-
 
 
 # Extract the 1st quartile
@@ -135,6 +70,89 @@ print("76% - 100% range:", range_76_100)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+def calculate_pageranks():
+
+    # Calculate the page ranking
+    # TODO: pagerank = g.pagerank(damping=0.85)?
+    pagerank = g.pagerank()
+
+
+    # Pair ranks with usernames
+    pagerank_list = []
+    for vertex, score in zip(g.vs, pagerank):
+        pagerank_list.append((vertex['name'], score))
+
+
+    # Sort the list
+    # TODO: ascending or descending? -> # sorted_scores = sorted(pageranks_map.items(), key=lambda item: item[1], reverse=True)
+    sorted_pageranks_list = sorted(pagerank_list, key=lambda item: item[1])
+
+
+
+    scores = [score for name, score in sorted_pageranks_list]
+
+    # Calculate percentiles
+    p25 = np.percentile(scores, 25)
+    p50 = np.percentile(scores, 50)
+    p75 = np.percentile(scores, 75)
+
+    # Initialize lists for each range
+    range_0_25 = []
+    range_26_50 = []
+    range_51_75 = []
+    range_76_100 = []
+
+    # Assign data to each range based on scores
+    for name, score in sorted_pageranks_list:
+        if score <= p25:
+            range_0_25.append((name, score))
+        elif p25 < score <= p50:
+            range_26_50.append((name, score))
+        elif p50 < score <= p75:
+            range_51_75.append((name, score))
+        elif score > p75:
+            range_76_100.append((name, score))
+
+    # Display the results
+    print("0% - 25% range:", range_0_25)
+    print("26% - 50% range:", range_26_50)
+    print("51% - 75% range:", range_51_75)
+    print("76% - 100% range:", range_76_100)
+
+
+
+
+
+
+
+
+
+
+def main():
+    # Initialize an empty list to store edges
+    edges = []
+
+
+    # Read the CSV file
+    with open('', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            edges.append(tuple(row))
+
+
+    # Create a graph from the list of edges
+    g = ig.Graph.TupleList(edges, directed=True)
 
 
 
