5e2b2206140ad0b691e970c0609d8ae62466e1fc
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-25 21:02:05 +0200

    changes

--- src/tmp.py
+++ src/tmp.py
@@ -5,7 +5,7 @@ import csv
 
 
 
-def read_csv(filename):
+def read_dict_csv(filename):
 
     # Dictionary to store the data
     user_status_dict = {}
@@ -28,7 +28,7 @@ def read_csv(filename):
 
 
 
-def write_csv(list):
+def write_dict_csv(list):
     with open('complete_list.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Username", "Parsed Status"])  # Writing headers
@@ -38,28 +38,74 @@ def write_csv(list):
 
 
 
+
+
+def read_network_csv(file_path):
+    # List to store the tuples
+    data_tuples = []
+
+    # Reading the CSV file
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip the header
+        for row in csv_reader:
+            data_tuples.append((row[0], row[1]))  # Append each row as a tuple to the list
+
+    return data_tuples
+
+
+
+
+
+def write_network_csv(network):
+     with open('complete_network.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Source", "Destination"])
+        # Write each tuple in the network list to the file
+        for connection in network:
+            writer.writerow(connection)
+
+
+
+
+
+
 def main(path1, path2):
 
-    dict1 = read_csv(path1)
-
-    dict2 = read_csv(path2)
-
-    dict1.update(dict2)
-
-    write_csv(dict1)
+    # Dictionary execution
+    # dict1 = read_csv(path1)
+    # dict2 = read_csv(path2)
+    # dict1.update(dict2)
+    # write_csv(dict1)
 
 
+    # Network execution
+    network1 = read_network_csv(path1)
+    network2  = read_network_csv(path2)
+    network1.extend(network2)
+    write_network_csv(network1)
+
+
+    print("Program ended succesfully")
 
 
 
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+main("./network2.csv", "Sample_User_Network.csv")
 
-    parser.add_argument("list1")
-    parser.add_argument("list2")
 
-    args = parser.parse_args()
 
-    main(args.list1, args.list2)
+
+
+
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+
+#     parser.add_argument("list1")
+#     parser.add_argument("list2")
+
+#     args = parser.parse_args()
+
+#     main(args.list1, args.list2)
